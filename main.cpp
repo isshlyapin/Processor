75ec94b52f048675cf1d14652ec6233a05c9472f
admin <shlyapin2005@gmail.com>
2023-10-07 12:03:39 +0300

    Первый вариант процессора со strcmp

--- main.cpp
+++ main.cpp
@@ -5,83 +5,153 @@
 
 const char *commands[] = {
     "push", // 0
-    "sqrt", // 1
-    "sin",  // 2
-    "cos",  // 3
+    "c_sqrt", // 1
+    "c_sin",  // 2
+    "c_cos",  // 3
     "in",   // 4
     "add",  // 5
     "sub",  // 6
     "mul",  // 7
-    "div",  // 8
+    "c_div",  // 8
     "out",  // 9
     "hlt",  // 10
 };
 
-enum COMMANDS {  push,  sqrt,  sin,  cos,  in,
-                 add,   sub,   mul,  div,  out,
+enum COMMANDS {  push,  c_sqrt,  c_sin,  c_cos,  in,
+                 add,   sub,   mul,  c_div,  out,
                  hlt};
+
 const size_t size_comm = 100;
+const int COMPLETED = 0;
+
+typedef double NUM_T;
+#define NUM_MOD "%lf"
+
 
 int main()
 {
+  Stack stk = {};
+  StackCtor(&stk, 2);
+
   char command_char[size_comm] = "venom";
   int command = -9;
-  int     num =  0;
+  double  num =  0;
   
   FILE *fp_src = fopen("src.txt", "r");
-  assert(fscanf(fp_src, "%s", command_char) == 1);
-
-  size_t size_commands = sizeof(commands) / sizeof(commands[0]);
-  for (size_t i = 0; i < size_commands; i++)
-  {
-    if (!strcmp(command_char, commands[i]))
-      command = i;
-  }
-  switch (command)
-  {
-  case push:
-    fscanf(fp_src, "%d", &num);
-    break;
-  case sqrt:
-    
+  assert(fp_src != NULL);
+  // size_t size_commands = sizeof(commands) / sizeof(commands[0]);
   
-  default:
-    break;
-  }
-    bool command_with_num = 0;
-    bool command_in = 0;
-    
-    for (size_t i = 0; i < size_c_with_n; i++)
+  while(!feof(fp_src))
+  {
+    assert(fscanf(fp_src, "%s", command_char) == 1);
+    printf("[[%s]]\n", command_char);
+    for (size_t i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
+      if (!strcmp(command_char, commands[i]))
+        command = i;
+    // printf("%lu\n", sizeof(commands) / sizeof(commands[0]));    
+    printf("Number command:: %d\n", command);
+    NUM_T num2 = VENOM_ELEM;
+    NUM_T num1 = VENOM_ELEM;
+
+    switch (command)
     {
-      if (!strcmp(command, commands_with_num[i]))
-      {
-        if (!strcmp(command, commands_with_num[4]))
-          command_in = 1;
-        
-        command_with_num = 1;
+      case push:
+        fscanf(fp_src, NUM_MOD, &num);
+        printf("Numeric from file: <%lf>\n", num);         ////
+        StackPush(&stk, num);
+        printf("\n");
         break;
-      }
-    }
 
-    if (command_with_num)
-    {
-      if (command_in)
-      {
-        printf("Please, input num\n");
-        fscanf(stdin, "%d", &num);
-      }
-      else
-        fscanf(fp_src, "%d", &num);
+      case c_sqrt:
+        fscanf(fp_src, NUM_MOD, &num);
+        printf("Numeric from file: <%lf>\n", num);         ////
+
+        StackPush(&stk, sqrt(num));
+        printf("\n");
+        break;
+
+      case c_sin:
+        fscanf(fp_src, NUM_MOD, &num);
+        printf("Numeric from file: <%lf>\n", num);         ////
+   
+        StackPush(&stk, sin(num));
+        printf("\n");
+        break;
+      
+      case c_cos:
+        fscanf(fp_src, NUM_MOD, &num);
+        printf("Numeric from file: <%lf>\n", num);         ////
+
+        StackPush(&stk, cos(num));
+        printf("\n");
+        break;
+      
+      case in:
+        printf("Введите число: \n");
+        fscanf(stdin, NUM_MOD, &num);
+        printf("Numeric user: <%lf>\n", num);         ////
+
+        StackPush(&stk, num);
+        printf("\n");
+        break;
+      
+      case add:
+        // printf("Add:  \n");
+        StackPop(&stk, &num1);        
+        StackPop(&stk, &num2);
+
+        StackPush(&stk, num1 + num2);
+        printf("\n");
+        break;
+      
+      case sub:
+        // printf("Sub:  \n");
+
+        StackPop(&stk, &num1);
+        StackPop(&stk, &num2);
+
+        StackPush(&stk, num2 - num1);
+        printf("\n");
+        break;
+      
+      case mul:
+        // printf("Mul:  \n");
+
+        StackPop(&stk, &num1);
+        StackPop(&stk, &num2);
+
+        StackPush(&stk, num1 * num2);
+        printf("\n");
+        break;
+      
+      case c_div:
+        // printf("Div:  \n");
+
+        StackPop(&stk, &num1);
+        StackPop(&stk, &num2);
+
+        StackPush(&stk, num2 / num1);
+        printf("\n");
+        break;
+      
+      case out:
+        // printf("Out:  \n");
+
+        StackPop(&stk, &num1);
+
+        printf("Ответ: [" NUM_MOD "]\n", num1);
+        printf("\n");
+        break;
+
+      case hlt:
+        // printf("Hlt:  \n");
+
+        return COMPLETED;
+
+      default:
+        break;
     }
   }
-  printf("%d\n", num);
-  // printf("%s\n", command);
-  // printf("%s\n", commands[10]);
-  // printf("%d", strcmp(command, commands[10]));
 
-  if (!strcmp(command, commands[10]))
-    printf("working\n");
-
-  // while()
   fclose(fp_src);
 }
